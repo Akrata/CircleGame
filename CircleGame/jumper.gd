da8c58e675a6e4a8f3254e0d4ab78f0df0246881
Pablo <pablofeijo181@gmail.com>
2019-10-09 12:59:59 -0300

    primer video

---
+++ CircleGame/jumper.gd
@@ -0,0 +1,28 @@
+extends Area2D
+
+
+signal captured
+var velocity = Vector2(100, 0)
+var jump_speed = 1000
+var target = null
+
+func _unhandled_input(event):
+	if target and event is InputEventScreenTouch and event.is_pressed():
+		jump()
+		
+		
+func jump():
+	target = null
+	velocity = transform.x * jump_speed
+
+func _on_jumper_area_entered(area: Area2D) -> void:
+	target = area
+	target.get_node("Pivot").rotation = (position - target.position).angle()
+	velocity = Vector2.ZERO
+	emit_signal("captured", area)
+	
+func _physics_process(delta: float) -> void:
+	if target:
+		transform = target.orbit_position.global_transform
+	else:
+		position += velocity * delta
